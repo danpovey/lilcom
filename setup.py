a697049d80deab69693633447e7c6f0ba8cc0561
soroushzargar <soroushzargar@gmail.com>
2019-07-31 21:15:44 +0430

    changed to add numpy compatibility

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from distutils.core import setup, Extension
 
-extension_mod = Extension("lilcomlib", ["lilcommodule.c"])
+# TODO: Checking the version of python interpreter. This code only works with python2.
+
+from distutils.core import setup, Extension
+import numpy
+
+extension_mod = Extension("lilcomlib", ["lilcommodule.c"],include_dirs=[numpy.get_include()])
 
 setup(name = "lilcomlib", ext_modules=[extension_mod])
+
+
