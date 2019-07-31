ab007d77f7307e85dbd49fc280cea2276c492e1c
soroushzargar <soroushzargar@gmail.com>
2019-07-31 21:14:55 +0430

    compress function developed, data conversion done, two reamining things are manipulating floats and calling the lilcom_compress function

--- lilcommodule.c
+++ lilcommodule.c
@@ -1,21 +1,73 @@
 #include <Python.h>
-//#include "lilcom.h"
-
-char * hello(char * what) {
-  return "hello";
-}
+#include "numpy/arrayobject.h"
+#include "lilcom.h"
 
 static PyObject * compress(PyObject * self, PyObject * args)
 {
-  printf("Compress called with argument %d\n", &args[0]);
+  printf("DEBUG: Compress called \n");
   
-  Py_RETURN_NONE;
+  PyObject *signalObj;
+  if (!PyArg_ParseTuple(args, "O", &signalObj)) {
+      Py_RETURN_FALSE;
+  }
+  int n_samples = (int)PyArray_DIM(signalObj, 0);
+  int stride = (int)PyArray_DIM(signalObj, 1);
+
+  // There is a bug in the stride calculation
+  stride = 1;
+
+  printf("number of samples = %d\n", n_samples);
+  printf("stride = %d\n", stride);
+
+  int **singal_stream = (int **)(PyArray_DATA(signalObj));
+
+  // Conversion to int16_t
+  int16_t *input = malloc(sizeof(int16_t)* n_samples * stride);
+  for (int i = 0 ; i < n_samples * stride; i++){ 
+    printf("i: %d\t", i);
+    printf("%d\t",singal_stream[i]);
+    input[i] = singal_stream[i];
+    printf("%d\n", input[i]);
+  }
+
+  int8_t * output;
+  int output_stride = 1;
+  int lpc_order = 1;
+
+  // FUNCTION CALL HERE!
+  //lilcom_compress(n_samples, input, stride, output, output_stride, lpc_order);
+
+  for (int i = 0 ; i < n_samples * stride; i++){ 
+    printf("i: %d\t", i);
+    printf("%d\n", output[i]);
+  }
+  
+  printf("length = %d\n", n_samples);
+  printf("Data consists of %d\n", singal_stream[5]);
+  
+  Py_RETURN_TRUE;
 }
+
+
 
 static PyObject * decompress(PyObject * self, PyObject * args)
 {
-  printf("Decompress called with argument %d\n", &args[0]);
-  Py_RETURN_NONE;
+  printf("DEBUG: Decompress called\n");
+
+  PyObject *signal_obj;
+  if (!PyArg_ParseTuple(args, "O", &signal_obj)) {
+      Py_RETURN_FALSE;
+  }
+  int n_samples = (int)PyArray_DIM(signal_obj, 0);
+  int stride = (int)PyArray_DIM(signal_obj, 1);
+  
+ // PyObject * signal = PyArray_FROM_OTF(signalObj, NPY_INT, NPY_IN_ARRAY);
+  int ** singal_stream = (int **)(PyArray_DATA(signal_obj));
+
+  int converted_signal;
+
+  printf("length = %d\n", n_samples);
+  printf("Data consists of %d\n", singal_stream[120]);
 }
 
 
