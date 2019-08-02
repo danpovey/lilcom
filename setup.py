d718a6df5585f6f3539708799e748e43ef395cc5
soroushzargar <soroushzargar@gmail.com>
2019-08-02 20:58:29 +0430

    Problem, Segmentation Fault on function call.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup, Extension
 import numpy
 
-extension_mod = Extension("lilcomlib", ["lilcommodule.c"],include_dirs=[numpy.get_include()])
+extension_mod = Extension("lilcomlib", ["lilcommodule.c","lilcom.h"],include_dirs=[numpy.get_include()])
 
 setup(name = "lilcomlib", ext_modules=[extension_mod])
 
