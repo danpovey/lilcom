a74e1ab8cd9a35d2b8275a5c8de0b3ea00eaa958
soroushzargar <soroushzargar@gmail.com>
2019-08-05 00:39:10 +0430

    test file updated for decompression.

--- test.py
+++ test.py
@@ -1,15 +1,24 @@
-import lilcomlib
+import lilcom
 import numpy
+import csv
+
+# read CSV file & load into list
+with open("sample.txt", 'r') as my_file:
+    reader = csv.reader(my_file)
+    data = list(reader)
+
+
+data = [int(d) for d in data[0]]
+print(data)
 
 # Sample test array to test the function.
-numpy_array = numpy.array([[1]* 3]*100)
-
+numpy_array = numpy.array(data)
 # Running the function in lilcommodule.c
-val1 = lilcomlib.compress(numpy_array)
+val1 = lilcom.compress(numpy_array)
 
 # numpy_array = numpy.array([[5]*10] * 100)
-# val2 = lilcomlib.decompress(numpy_array)
+val2 = lilcom.decompress(val1)
 
 
 print (val1)
-#print (val2)
+print (val2)
