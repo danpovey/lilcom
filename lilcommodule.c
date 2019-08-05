e273fadbf702a0a0d320c932f1f891c2f0cbcb82
soroushzargar <soroushzargar@gmail.com>
2019-08-05 14:30:15 +0430

    Strides are received from numpy array, also supporting proper data type only

--- lilcommodule.c
+++ lilcommodule.c
@@ -38,186 +38,101 @@ int integral_check (int array_type){
 static PyObject * compress(PyObject * self, PyObject * args, PyObject * keywds)
 {
   /* Defining all variables */
-  PyObject *signal_object; // The input signal, passed as a numpy array.
+  PyObject *signal_input; // The input signal, passed as a numpy array.
+  PyObject *signal_output; // The output signal, passed as a numpy array.
   int n_dims = 0; // Number of dimensions of the given numpy array
   int input_stride; // The number of integers between to consecutive samples
   int output_stride; // The number of integers between to consecutive samples in output
   int n_samples = 0; // number of samples given in the numpy array
   // INPUT IS DEFINED INSIDE IF CONDITIONS
   int8_t *output; // The one dimensional vectorized output array which will be modified by the core function
+  int16_t *input; // The one dimensional vectorized input array which will be given to the core function
   int lpc_order = 5; // LPC Order defined in the core function (more information -> lilcom.h)
-  int integral = 1; // Checks whether the array contains integer or float
 
-  /* Reading and information - extracting for input data */
-  static char *kwlist[] = {"X" , "lpc_order", NULL};
-  if (!PyArg_ParseTupleAndKeywords(args, keywds, "O|i", kwlist, &signal_object, &lpc_order)) 
+  /* Reading and information - extracting for input data 
+    From the python function there are two numpy arrays and an intger (optional) LPC_order 
+    passed to this madule. Following part will parse the set of variables and store them in corresponding
+    objects.
+  */
+  static char *kwlist[] = {"X", "Y" , "lpc_order", NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, keywds, "OO|i", kwlist, &signal_input, &signal_output, &lpc_order)) 
     Py_RETURN_FALSE;
   
   // Initializing shape related variables
-  n_dims = PyArray_NDIM(signal_object); // Getting the number of dimensions
-  n_samples = PyArray_DIM(signal_object , 0); // Getting the first dimension
-  if (n_dims > 1) input_stride = PyArray_DIM(signal_object , 0); // Conditioning on second dimension
-  else input_stride = 1;
-  integral = integral_check(((PyArrayObject*)signal_object)->descr->type_num); // Checking whether the array has float
+  n_dims = PyArray_NDIM(signal_input); // Getting the number of dimensions
+  n_samples = PyArray_DIM(signal_input , 0); // Getting the first dimension
+  /* In cases that numpy array is not contiguous then stride values are not equal to 1
+    The stride values given in bytes and division to the size of variables gives the 
+    strides as blocks.
+   */
+  input_stride = PyArray_STRIDE(signal_input, 0)/sizeof(int16_t);
+  output_stride = PyArray_STRIDE(signal_output, 0)/sizeof(int8_t);
 
-
-  /* Debug: Comment or Uncomment when on debug */
-  // printf("The array is received and the lpc_order = %d\n", lpc_order);
-  // printf("Array is %d dimensional\n", n_dims);
-  // printf("It has %d samples and stride is %d\n", n_samples, input_stride);
-  // printf("PyArrayType == int is %d\n", integral );
-
-
-  output_stride = input_stride; ////// CHANGE IT IF NEEDED
+  /* Access to the data part of the numpy array */
+  input = PyArray_DATA(signal_input);
+  output = PyArray_DATA(signal_output);
   
-  /* Ensuring that the array is contiguous */
-  PyArrayObject *signal_object_contiguous = PyArray_GETCONTIGUOUS(signal_object); 
+  /* Calling the core function */
+  lilcom_compress(n_samples, input, input_stride, output, output_stride, lpc_order);
 
+  /* Making the resulting array */
+  npy_intp * output_dimensions = malloc(sizeof(npy_intp)*2);
+  output_dimensions[0] = n_samples;
+  output_dimensions[1] = output_stride;
+  PyArrayObject * output_array = (PyArrayObject *) PyArray_SimpleNewFromData(n_dims, output_dimensions, NPY_INT8, (void*) output);
 
-  void *signal_object_data = PyArray_DATA(signal_object_contiguous);
-
-  if(integral == 1) { // Integer
-    int16_t *input; // The one dimensional vectorized input array which will be given to the core function
-    /* Allocating the space for input and output */
-    input = malloc(sizeof(int16_t) * n_samples * input_stride);
-    output = malloc(sizeof(int8_t) * n_samples * output_stride);
-    
-    for (int i = 0 ; i < n_samples * input_stride ; i++){
-          input[i] = (int16_t)(((int16_t *)signal_object_data)[i]); 
-    }     
-
-    /* Calling the core function */
-    lilcom_compress(n_samples, input, input_stride, output, output_stride, lpc_order);
-
-    /* Debug: Comment or Uncomment when on debug */
-    // for (int i = 0; i < n_samples * input_stride ; i++){
-    //   printf("for index %d a = %d and b = %d\n", i , input[i], output[i]);
-    // }
-
-    /* Making the resulting array */
-    npy_intp * output_dimensions = malloc(sizeof(npy_intp)*2);
-    output_dimensions[0] = n_samples;
-    output_dimensions[1] = output_stride;
-    PyArrayObject * output_array = (PyArrayObject *) PyArray_SimpleNewFromData(n_dims, output_dimensions, NPY_INT8, (void*) output);
-
-    /* Overcoming memory leak problem */
-    free(input);
-    /* Returning numpy array */
-    PyObject *returner = PyArray_Return(output_array);
-    return returner;
-  } 
-  
-  else { // Float
-    return NULL;
-  //   float *input; // The one dimensional vectorized input array which will be given to the core function
-  //   /* Allocating the space for input and output */
-  //   input = malloc(sizeof(float) * n_samples * input_stride);
-  //   output = malloc(sizeof(int8_t) * n_samples * output_stride);
-    
-
-  //   /* Conversion to int16_t array */
-  //   switch (((PyArrayObject*)signal_object)->descr->type_num){
-  //     case NPY_FLOAT:
-  //       for (int i = 0 ; i < n_samples * input_stride ; i++){
-  //         input[i] = (float)(((float *)signal_object_data)[i]); 
-  //       } break;
-  //     case NPY_DOUBLE:
-  //       for (int i = 0 ; i < n_samples * input_stride ; i++){
-  //         input[i] = (float)(((double *)signal_object_data)[i]); 
-  //       } break;
-  //     case NPY_LONGDOUBLE:
-  //       for (int i = 0 ; i < n_samples * input_stride ; i++){
-  //         input[i] = (float)(((long double *)signal_object_data)[i]); 
-  //       } break;
-  //   }
-
-  //   /* Calling the core function */
-  //   //lilcom_compress_float(n_samples, input, input_stride, output, output_stride);
-
-  //   /* Debug: Comment or Uncomment when on debug */
-  //   // for (int i = 0; i < n_samples * input_stride ; i++){
-  //   //   printf("for index %d a = %f and b = %d\n", i , input[i], output[i]);
-  //   // }
-
-  //   /* Making the resulting array */
-  //   npy_intp * output_dimensions = malloc(sizeof(npy_intp)*2);
-  //   output_dimensions[0] = n_samples;
-  //   output_dimensions[1] = output_stride;
-  //   PyArrayObject * output_array = (PyArrayObject *) PyArray_SimpleNewFromData(n_dims, output_dimensions, NPY_INT8, (void*) output);
-
-  //   /* Overcoming memory leak problem */
-  //   free(input);
-  //   /* Returning numpy array */
-  //   PyObject *returner = PyArray_Return(output_array);
-  //   return returner;
-  }
-  return NULL;
+  // /* Returning numpy array */
+  PyObject *returner = PyArray_Return(output_array);
+  return returner;
 }
 
 
 
 static PyObject * decompress(PyObject * self, PyObject * args,  PyObject * keywds)
 {
-  /* Defining all variables */
-  PyObject *signal_object; // The input signal, passed as a numpy array.
+   /* Defining all variables */
+  PyObject *signal_input; // The input signal, passed as a numpy array.
+  PyObject *signal_output; // The output signal, passed as a numpy array.
   int n_dims = 0; // Number of dimensions of the given numpy array
   int input_stride; // The number of integers between to consecutive samples
   int output_stride; // The number of integers between to consecutive samples in output
   int n_samples = 0; // number of samples given in the numpy array
   // INPUT IS DEFINED INSIDE IF CONDITIONS
   int16_t *output; // The one dimensional vectorized output array which will be modified by the core function
-
-  /* Reading and information - extracting for input data */
-  static char *kwlist[] = {"X" , NULL};
-  if (!PyArg_ParseTupleAndKeywords(args, keywds, "O", kwlist, &signal_object)) 
+  int8_t *input; // The one dimensional vectorized input array which will be given to the core function
+  /* Reading and information - extracting for input data 
+    From the python function there are two numpy arrays and an intger (optional) LPC_order 
+    passed to this madule. Following part will parse the set of variables and store them in corresponding
+    objects.
+  */
+  static char *kwlist[] = {"X", "Y", NULL};
+  if (!PyArg_ParseTupleAndKeywords(args, keywds, "OO", kwlist, &signal_input, &signal_output)) 
     Py_RETURN_FALSE;
   
   // Initializing shape related variables
-  n_dims = PyArray_NDIM(signal_object); // Getting the number of dimensions
-  n_samples = PyArray_DIM(signal_object , 0); // Getting the first dimension
-  if (n_dims > 1) input_stride = PyArray_DIM(signal_object , 0); // Conditioning on second dimension
-  else input_stride = 1;
+  n_dims = PyArray_NDIM(signal_input); // Getting the number of dimensions
+  n_samples = PyArray_DIM(signal_input , 0); // Getting the first dimension
+  /* In cases that numpy array is not contiguous then stride values are not equal to 1
+    The stride values given in bytes and division to the size of variables gives the 
+    strides as blocks.
+   */
+  input_stride = PyArray_STRIDE(signal_input, 0)/sizeof(int8_t);
+  output_stride = PyArray_STRIDE(signal_output, 0)/sizeof(int16_t);
 
-
-  /* Debug: Comment or Uncomment when on debug */
-  // printf("Array is %d dimensional\n", n_dims);
-  // printf("It has %d samples and stride is %d\n", n_samples, input_stride);
-
-
-  output_stride = input_stride; ////// CHANGE IT IF NEEDED
-
-  /* Ensuring that the array is contiguous */
-  PyArrayObject *signal_object_contiguous = PyArray_GETCONTIGUOUS(signal_object);
-
-  void *signal_object_data = PyArray_DATA(signal_object_contiguous);
-
-
-  int8_t *input; // The one dimensional vectorized input array which will be given to the core function
-  /* Allocating the space for input and output */
-  input = malloc(sizeof(int8_t) * n_samples * input_stride);
-  output = malloc(sizeof(int16_t) * n_samples * output_stride);
+  /* Access to the data part of the numpy array */
+  input = PyArray_DATA(signal_input);
+  output = PyArray_DATA(signal_output);
   
-  for (int i = 0 ; i < n_samples * input_stride ; i++){
-        input[i] = (int8_t)(((int8_t *)signal_object_data)[i]); 
-  }
-
   /* Calling the core function */
   lilcom_decompress(n_samples, input, input_stride, output, output_stride);
-
-  /* Debug: Comment or Uncomment when on debug */
-  // for (int i = 0; i < n_samples * input_stride ; i++){
-  //   printf("for index %d a = %d and b = %d\n", i , input[i], output[i]);
-  // }
 
   /* Making the resulting array */
   npy_intp * output_dimensions = malloc(sizeof(npy_intp)*2);
   output_dimensions[0] = n_samples;
   output_dimensions[1] = output_stride;
-  PyArrayObject * output_array = (PyArrayObject *) PyArray_SimpleNewFromData(n_dims, output_dimensions, NPY_INT16, (void*) output);
+  PyArrayObject * output_array = (PyArrayObject *) PyArray_SimpleNewFromData(n_dims, output_dimensions, NPY_INT8, (void*) output);
 
-  /* Overcoming memory leak problem */
-  free(input);
-  /* Returning numpy array */
+  // /* Returning numpy array */
   PyObject *returner = PyArray_Return(output_array);
   return returner;
 
