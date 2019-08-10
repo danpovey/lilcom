9fde26a1329285a764cda9656f8adcaff4cd98d1
soroushzargar <soroushzargar@gmail.com>
2019-08-10 12:14:37 +0430

    added test for linear one dimensional integer array

---
+++ test/routine1-one-dimensional-integer.py
@@ -0,0 +1,23 @@
+# Signals are supposed to be given as numpy arrays
+import numpy
+# The main library
+import lilcom
+# To create random samples for the signal
+import random
+
+n_samples = 1000
+
+inputArray = numpy.array([random.randrange(-(2**15), 2**15 - 1) for i in range (n_samples)]).astype(numpy.int16)
+outputArray = numpy.zeros(inputArray.shape, numpy.int8)
+reconstruction = numpy.zeros(inputArray.shape, numpy.int16)
+
+lilcom.compress16i_8i(inputArray, outputArray)
+
+lilcom.decompress(outputArray, reconstruction)
+
+for i in range(n_samples):
+        print("Sample no ", i , "original number = ", inputArray[i], \
+                " compressed = ", outputArray[i], " reconstructed number = ", reconstruction[i])
+
+
+
